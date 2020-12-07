bff37d202c44756868cc77ee1d38c7c65003b6cd
alexeyasvarisch <Asvarischalex@gmail.com>
2020-12-07 23:23:51 +0200

    finished

--- src/main/java/telran/ashkelon0/service/SearcherService.java
+++ src/main/java/telran/ashkelon0/service/SearcherService.java
@@ -1,16 +1,10 @@
 package telran.ashkelon0.service;
 
 
-import org.elasticsearch.common.unit.DistanceUnit;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 import org.springframework.data.geo.Distance;
-import org.springframework.data.geo.GeoResults;
-import org.springframework.data.geo.Metrics;
 import org.springframework.data.geo.Point;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -23,18 +17,14 @@ import telran.ashkelon0.dao.LostFoundRepository;
 import telran.ashkelon0.dto.KafkaPostDto;
 import telran.ashkelon0.model.ElasticPost;
 
-import telran.ashkelon0.model.Location;
-
 
 @Component
 public class SearcherService {
 	
 	private static final String LOST_FOUND_TOPIC = "ir4jztza-lostfound";
-	private static final String SEARCH_RESULTS_TOPIC = "ir4jztza-searchresult";
-	
 
-	@Autowired
-	KafkaTemplate<String, String> kafkaTemplate;
+//	@Autowired
+//	KafkaTemplate<String, String> kafkaTemplate;
 	
 	@Autowired
 	LostFoundRepository repository;
@@ -50,7 +40,7 @@ public class SearcherService {
 	
 	
 	@KafkaListener(topics = LOST_FOUND_TOPIC)
-	public void dispatchPostsIntoDifferentTopics(String postJson) {
+	public void reciveLFpostsFromKarafka(String postJson) {
 		try {
 			KafkaPostDto postDto =  mapper.readValue(postJson, KafkaPostDto.class);
 			Point location = new Point(postDto.getLocation().getLatitude(), postDto.getLocation().getLongitude());
@@ -62,14 +52,14 @@ public class SearcherService {
 			if (post.isTypePost()) {
 				SearchHits<ElasticPost> returnedPosts = repository.findByTypePostAndTypeAndTagsInAndLocationNear(false, post.getType(), post.getTags(), location, distance);
 				emailService.sendSimpleMessage(post.getUserLogin(), returnedPosts);
-//				returnedPosts.forEach(System.out::println);
-//				System.out.println("///////////////////////////");
+				returnedPosts.forEach(System.out::println);
+				System.out.println("///////////////////////////");
 				
 			} else {
 				SearchHits<ElasticPost> returnedPosts = repository.findByTypePostAndTypeAndTagsInAndLocationNear(true, post.getType(), post.getTags(), location, distance);
 				emailService.sendSimpleMessage(post.getUserLogin(), returnedPosts);
-//				returnedPosts.forEach(System.out::println);
-//				System.out.println("///////////////////////////");
+				returnedPosts.forEach(System.out::println);
+				System.out.println("///////////////////////////");
 			}
 			
 		} catch (JsonProcessingException e) {
